ee6671ca78f1db90fd7b65a34206f6831573dcb1
FSanchez <anfelsanchez@gmail.com>
2019-10-26 12:40:49 -0500

    Creacion clase Luz

---
+++ app/src/main/java/com/example/parcial2/Clases/Luz.java
@@ -0,0 +1,31 @@
+package com.example.parcial2.Clases;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Luz {
+    public String locacion;
+    public boolean estado;
+
+    public Luz(String locacion, boolean estado){
+        locacion = locacion;
+        estado = estado;
+    }
+
+    public List<Luz> crearListaFromJSON(JSONObject jsonObject) throws JSONException {
+        List<Luz> listaLuces = new ArrayList<>();
+        Iterator<String> keys = jsonObject.keys();
+
+        while(keys.hasNext()) {
+            String key = keys.next();
+            if (jsonObject.get(key) instanceof JSONObject) {
+                listaLuces.add(new Luz(key, jsonObject.getBoolean(key)));
+            }
+        }
+        return listaLuces;
+    }
+}
