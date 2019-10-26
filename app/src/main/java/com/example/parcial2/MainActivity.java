98b6693d96a138ae4e2296150c2d900b123657d0
FSanchez <anfelsanchez@gmail.com>
2019-10-26 13:30:42 -0500

    peticion get

--- app/src/main/java/com/example/parcial2/MainActivity.java
+++ app/src/main/java/com/example/parcial2/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.parcial2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.example.parcial2.Clases.Luz;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView hab4;
     private ImageView bath;
     private ImageView kitch;
-
+    public List<Luz> arreglo = new ArrayList<>();
 
 
     @Override
@@ -35,22 +49,24 @@ public class MainActivity extends AppCompatActivity {
         kitch = findViewById(R.id.kitch);
 
 
-        List<Luz> arreglo = new ArrayList<>();
+
 
         arreglo.add(new Luz("luzBano", true));
         arreglo.add(new Luz("luzCocina", true));
         arreglo.add(new Luz("luzHabitacion1", true));
-        arreglo.add(new Luz("luzHabitacion2", true));
-        arreglo.add(new Luz("luzHabitacion3", true));
+        arreglo.add(new Luz("luzHabitacion2", false));
+        arreglo.add(new Luz("luzHabitacion3", false));
         arreglo.add(new Luz("luzHabitacion4", true));
+        setAllImages();
 
+    }
+    public void setAllImages(){
         setImage(arreglo.get(0),bath);
         setImage(arreglo.get(1),kitch);
         setImage(arreglo.get(2),hab1);
         setImage(arreglo.get(3),hab2);
         setImage(arreglo.get(4),hab3);
         setImage(arreglo.get(5),hab4);
-
     }
         public void setImage(Luz luz, ImageView imgView){
 
@@ -60,4 +76,59 @@ public class MainActivity extends AppCompatActivity {
                 imgView.setImageResource(R.drawable.off);
             }
         }
+
+
+    public void getCurrentStatus(String serviceEndPoint){
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    URL urlService = new URL ("http://192.168.180.26:3000/aptos/aptoStatus" );
+                    HttpsURLConnection connection =  (HttpsURLConnection) urlService.openConnection();
+                    connection.setRequestMethod("GET");
+                    InputStream responseBody = connection.getInputStream();
+
+                    if (connection.getResponseCode() == 200) {
+                        // Success
+                        InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
+                        BufferedReader streamReader = new BufferedReader(responseBodyReader);
+                        StringBuilder responseStrBuilder = new StringBuilder();
+
+                        String inputStr;
+                        while ((inputStr = streamReader.readLine()) != null)
+                            responseStrBuilder.append(inputStr);
+
+                        JSONObject jsonObject = new JSONObject(responseStrBuilder.toString());
+                        arreglo = crearListaFromJSON(jsonObject);
+                        setAllImages();
+                    } else {
+                        // Error handling code goes here
+                        Log.v("ERROR", "ERROR");
+                    }
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
+    public List<Luz> crearListaFromJSON(JSONObject jsonObject) throws JSONException {
+        List<Luz> listaLuces = new ArrayList<>();
+        Iterator<String> keys = jsonObject.keys();
+
+        while(keys.hasNext()) {
+            String key = keys.next();
+            if (jsonObject.get(key) instanceof JSONObject) {
+                listaLuces.add(new Luz(key, jsonObject.getBoolean(key)));
+            }
+        }
+        return listaLuces;
+    }
+}
